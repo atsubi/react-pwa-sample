0c4646ac15cdf92566657f25c738a8b177b94f45
atsubi <wisemagic51@gmail.com>
2024-12-31 02:29:30 +0000

    first react pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['images/apple-touch-icon.png'],
+      manifest: {
+        name: 'React PWA Sample',
+        short_name: 'PWA',
+        description: 'PWAサンプル',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'assets/192_noa.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'assets/512_noa.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })],
 })
