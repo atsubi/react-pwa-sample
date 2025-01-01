3acaf9a32e47ffc76d2efd22711fbc0f531c53b2
atsubi <wisemagic51@gmail.com>
2025-01-01 06:11:52 +0000

    sample form text 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [text, setText] = useState("ノア");
 
   return (
     <>
       <div>
         <form onSubmit={(e) => e.preventDefault()}>
-          <input type="text" value="" onChange={(e) => e.preventDefault()}/>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
           <input type="submit" value="編集" onSubmit={(e) => e.preventDefault()}/>
         </form>
       </div>
+
+      <p>{text}</p>
+
     </>
   );
 }
