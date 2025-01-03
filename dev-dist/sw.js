fe1d3c24c60995bb053ddbcc21b3ccf8d0f5bca5
atsubi <wisemagic51@gmail.com>
2025-01-03 05:16:36 +0000

    webcam 6 Change to camera Pro

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.btlocck0c5g"
+    "revision": "0.kf4e5hqa6qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/WebcamPro.tsx
+++ src/WebcamPro.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import {Camera, CameraType} from "react-camera-pro";
 
 export default function WebcamPro() {
