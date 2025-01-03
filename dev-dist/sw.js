1a1e8bca23bbcc6446269a3442ae7fde0affa9c9
atsubi <wisemagic51@gmail.com>
2025-01-03 05:37:08 +0000

    webcam 7 switch camera Pro

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kf4e5hqa6qg"
+    "revision": "0.nrco4c1mcgo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/WebcamPro.tsx
+++ src/WebcamPro.tsx
@@ -2,7 +2,7 @@ import { useState, useRef } from "react";
 import {Camera, CameraType} from "react-camera-pro";
 
 export default function WebcamPro() {
-    //const [cameras, setCameras] = useState<MediaDeviceInfo[]>();
+    const [cameras, setCameras] = useState(0);
     //const [camera, setCamera] = useState<MediaDeviceInfo>();
 
     const cameraProRef = useRef<CameraType>(null);
@@ -21,14 +21,18 @@ export default function WebcamPro() {
 
     return (
         <div>
-            <Camera ref={cameraProRef} errorMessages={{
+            <Camera ref={cameraProRef}
+                aspectRatio={4 / 3}
+                numberOfCamerasCallback={setCameras}
+                errorMessages={{
                 noCameraAccessible: undefined,
                 permissionDenied: undefined,
                 switchCamera: undefined,
                 canvas: undefined
             }} />
-            <button onClick={() => setImage(cameraProRef.current?.takePhoto() as string)}>Take Photo</button>
             {image && <img src={image} alt="Taken Photo" />}
+            <button onClick={() => setImage(cameraProRef.current?.takePhoto() as string)}>Take Photo</button>
+            <button hidden={cameras <= 1} onClick={() => cameraProRef.current?.switchCamera()}>Switch</button>
         </div>
     );
 }
