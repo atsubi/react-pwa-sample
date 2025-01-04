ad11c9b7c6c3afa637c9ef842fe422a064c886fa
atsubi <wisemagic51@gmail.com>
2025-01-04 02:01:48 +0000

    tailwind css 2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.42h7ht6oa"
+    "revision": "0.021s1s6dumg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import WebcamCamera from "./WebcamCamera.tsx";
+import { FileText } from "lucide-react";
 
 function App() {
   return (
-    <div className="flex flex-col">
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <header className="bg-white shadow">
+        <div className="text-3xl font-bold text-gray-900 flex items-center">
+          <FileText />
+          <h1>練習用アプリ</h1>
+        </div>
+      </header>
       <WebcamCamera />
     </div>
   )
