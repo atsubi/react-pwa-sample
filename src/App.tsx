18d9c61510401c3575381cd4422d6dc1b29935df
atsubi <wisemagic51@gmail.com>
2025-01-05 21:01:05 +0000

    add camera css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import WebcamCamera from "./WebcamCamera.tsx";
-import { FileText } from "lucide-react";
+import { useState } from "react";
+import { WebcamCamera } from "./WebcamCamera.tsx";
+import { Camera, FileText, Upload } from "lucide-react";
 
 function App() {
+  const [showCamera, setShowCamera] = useState<boolean>(false);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+
+  const handleCapture = (ImageData: string) => {
+    setCurrentImage(ImageData);
+    setShowCamera(false);
+  }
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
-      <header className="bg-white shadow">
-        <div className="text-3xl font-bold text-gray-900 flex items-center">
-          <FileText />
-          <h1>練習用アプリ</h1>
+    <div className="container border bg-gray-200 flex flex-col">
+      <header className="bg-white shadow mb-6">
+        <div className="max-w-7xl mx-auto px-4 py-6">
+          <h1 className="text-3xl font-bold flex items-center">
+            <FileText className="mr-2"/>
+            練習用アプリ
+          </h1>
         </div>
       </header>
-      <WebcamCamera />
+      <main className="max-w-7xl mx-auto bg-green-200 flex-grow px-4 py-6">
+        <div className="flex flex-col items-stretch">
+          <button onClick={() => setShowCamera(true)} className="flex items-center px-4 py-2 bg-blue-400 text-white rounded-lg hover:bg-blue-700 ">
+            <Camera className="mr-2" />
+            写真をとる
+          </button> 
+          <button onClick={(e) => e.preventDefault()} className="flex items-center px-4 py-2 bg-red-400 text-white rounded-lg hover:bg-red-700 ">
+            <Upload className="mr-2" />
+            画像・PDFをアップロードする
+          </button>        
+        </div>
+        {showCamera && (<WebcamCamera onCapture={handleCapture} onClose={() => setShowCamera(false) }/>)}
+        {currentImage && (<img src={currentImage} alt="" />) }
+      </main>
+      
     </div>
   )
 }
--- src/WebcamCamera.tsx
+++ src/WebcamCamera.tsx
@@ -1,20 +1,22 @@
 /**
  * React Webcamを使った撮影クラス
  */
-import { useState, useRef, useCallback } from "react";
+import React, { useRef, useCallback } from "react";
 import Webcam from "react-webcam";
-import { Camera } from "lucide-react";
+import { Camera, X } from "lucide-react";
 import { isMobile } from "react-device-detect";
 
-export default function WebcamCamera() {
+interface CameraProps {
+  onCapture: (ImageData: string) => void;
+  onClose: () => void;
+}
+
+export const WebcamCamera: React.FC<CameraProps> = ({onCapture, onClose}) => {
 
     //const [cameras, setCameras] = useState<MediaDeviceInfo[]>();
     //const [camera, setCamera] = useState<MediaDeviceInfo>();
 
     const webcamRef = useRef<Webcam>(null);
-    const [imgSrc, setImgSrc] = useState<string | null>("");
-
-    const [showCamera, setShowCamera] = useState<boolean>(true);
 
   // 背面カメラを設定
   /*useEffect(() => {
@@ -31,26 +33,23 @@ export default function WebcamCamera() {
     if (webcamRef.current != null) {
       const imageSrc = webcamRef.current?.getScreenshot();
       if (imageSrc) {
-        setImgSrc(imageSrc);
-      }
-      setShowCamera(false);
+        onCapture(imageSrc);
+      }      
     }
-  }, [webcamRef]);
+  }, [onCapture]);
 
   return (
-    <>
-        <main className="max-w-7xl">
-            {showCamera && (
-                <><Webcam
-                    audio={false}
-                    videoConstraints={(isMobile) ? { facingMode: { exact: "environment" } } : { facingMode: "user" }}
-                    ref={webcamRef}
-                    screenshotFormat="image/jpeg" />
-                <Camera onClick={capture} className="absolute bottom-8 left-1/2 transform -translate-x-1/2 bg-white rounded-full p-4">シャッター</Camera>
-                </> 
-            ) }
-            {imgSrc && <img src={imgSrc} />}
-        </main>
-    </>
+    <div className="fixed inset-0 bg-black z-50 flex flex-col">
+      <button onClick={onClose} className="absolute top-4 right-4 text-white p-2">
+        <X size={24}/>
+      </button>
+      <Webcam
+        audio={false}
+        videoConstraints={(isMobile) ? { facingMode: { exact: "environment" } } : { facingMode: "user" }}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg" 
+        className="w-full h-full object-cover"/>
+      <Camera onClick={capture} className="absolute bottom-8 left-1/2 transform -translate-x-1/2 bg-white rounded-full p-4"  size={32}/>
+    </div>
   );
 }
